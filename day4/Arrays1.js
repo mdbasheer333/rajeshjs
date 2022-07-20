7a7f1c4ad3147f761941e9195c7d3ea7fece641a
mdbasheer333 <mdbasheer333@gmail.com>
2022-07-20 08:23:41 +0530

    day4

---
+++ day4/Arrays1.js
@@ -0,0 +1,26 @@
+//var arr=[1,2,3,4,5,6,7,8];
+
+// arr.forEach((e)=>{
+//     console.log(e);
+// });
+
+// arr.forEach((ele, index, arr)=>{
+//     console.log(index + " : " + ele);
+// });
+
+// arr.filter((ele)=>ele%2==0).forEach((ele)=>{
+//     console.log(ele);
+// })
+
+
+// var str=["apple", "banana", "orange","abcd","grapes"]
+
+// str.filter((ele)=>ele.toString().startsWith("a")).forEach((e)=>{
+//     console.log(e);
+// });
+
+// var arr=[1,2,3,4,5,6,7,8];
+// arr.map((ele)=>ele*ele).forEach((ele)=>{
+//     console.log(ele);
+// });
+
